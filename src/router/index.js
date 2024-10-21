8d37f99601cf7e31de5ca4829131309fa56062bc
Martin Zuñiga <149126663+MartinZuniga-Nane@users.noreply.github.com>
2024-10-20 23:58:42 -0300

    Creacion Ranking

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,15 @@ import { createRouter, createWebHistory } from 'vue-router'
 import LoginContainer from '../components/LoginContainer.vue'
 import RegisterContainer from '../components/RegisterContainer.vue'
 import MainMenu from '../components/MainMenu.vue'
+import RankingContainer from '../components/RankingContainer.vue'
+
 
 const routes = [
   {path: '/login', name: 'LoginContainer', component: LoginContainer},
   {path : '/register', name: 'RegisterContainer', component: RegisterContainer},
-  {path: '/main', name: 'MainMenu', component: MainMenu}
+  {path: '/main', name: 'MainMenu', component: MainMenu},
+  {path: '/ranking', name: 'RankingContainer', component: RankingContainer},
+
 ]
 
 const router = createRouter({
