bb14f5537fcda15ab53d1a3a61edd63f7a31146c
Martin Zuñiga <149126663+MartinZuniga-Nane@users.noreply.github.com>
2024-10-20 23:36:27 -0300

    Creacion Login

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import LoginContainer from '../components/LoginContainer.vue'
 
-const routes = []
+const routes = [
+  {path: '/', name: 'LoginContainer', component: LoginContainer}
+]
 
 const router = createRouter({
   history: createWebHistory(),
