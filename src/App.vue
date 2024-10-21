bb09ca86b05270f6efbe74586e545886883f052f
iiesteban <e.ortiz06@ufromail.cl>
2024-10-20 21:09:50 -0300

    Add proyecto vue base
    Agrego proyecto base para todos los integrantes

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="h-screen flex justify-center items-center bg-slate-900">
+    <h1 class="text-white text-3xl">Hola, esta es la base</h1>
+  </div>
+</template>
+
+<script>
+</script>
