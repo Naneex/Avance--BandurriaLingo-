0b40c65c62d3584a62facda69022d8bff1426b5c
iiesteban <e.ortiz06@ufromail.cl>
2024-10-20 21:20:43 -0300

    Arreglo carpeta router

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,10 @@
-
 import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = []
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes,
+  history: createWebHistory(),
+  routes
 })
 
 export default router
