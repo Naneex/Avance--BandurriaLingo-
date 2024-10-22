3a8755b3a503c6849a3a97243a279e14c1dc25e0
iiesteban <e.ortiz06@ufromail.cl>
2024-10-21 21:27:48 -0300

    Creación funcionalidad chat
    Creación de la funcionalidad chat, la cual contiene un botón de traducción en tiempo real con otros usuarios.

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import ChatWeb from "../views/ChatWeb.vue";
 
-const routes = []
+const routes = [
+  { path: '/chat', component: ChatWeb},
+]
 
 const router = createRouter({
   history: createWebHistory(),
