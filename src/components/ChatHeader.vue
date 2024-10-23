1db6e27d68046d701c5ce6f1b9d87ba0d2823a03
iiesteban <e.ortiz06@ufromail.cl>
2024-10-22 23:39:54 -0300

    Agrego rutas para la funcionalidad chat
    Ahora se puede ir al chat con el usuario que se habla.

--- src/components/ChatHeader.vue
+++ src/components/ChatHeader.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="flex items-center py-2 px-5 justify-between shadow-sm bg-white rounded-xl ">
         <a href="">
+          <router-link :to="`/profile/${props.usuarioHablarId}`">
           <img :src="imagen" alt="" class="w-14 h-14 rounded-full">
+          </router-link>
         </a>
         <h2 class="font-semibold">{{nombre}}</h2>
         <a href="">
@@ -15,6 +17,7 @@
 
 const props = defineProps ({ 
   nombre: String,
-  imagen: String
+  imagen: String,
+  usuarioHablarId: String
 });
 </script>
