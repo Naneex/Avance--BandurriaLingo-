249f35893e2d09c49533367897561c57e05d52c3
Martin Zuñiga <martinzunigacampos@gmail.com>
2024-11-14 01:55:42 -0300

    feat: add Login

--- backend/src/main/java/BandurriaLingo/backend/auth/AuthService.java
+++ backend/src/main/java/BandurriaLingo/backend/auth/AuthService.java
@@ -1,6 +1,9 @@
 
 package BandurriaLingo.backend.auth;
 
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -17,9 +20,15 @@ class AuthService {
     private final UsuarioRepository userRepository;
     private final JwtService jwtService;
     private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
 
-    public AuthResponse login (LoginRequest loginRequest) {
-        return null;
+    public AuthResponse login(LoginRequest request) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getUsuario(), request.getContrasena()));
+        UserDetails user= userRepository.findByUsuario(request.getUsuario()).orElseThrow();
+        String token = jwtService.getToken(user);
+        return AuthResponse.builder()
+            .token(token)
+            .build();
     }
 
     public AuthResponse register (RegisterRequest request) {
